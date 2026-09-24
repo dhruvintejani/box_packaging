e412b712a91a38b19b91185ac0a43e1d9a7084c7
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-09-24 16:11:48 +0530

    Add keyboard skip target and correct mobile header offset on Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -132,7 +132,7 @@ export default function Products() {
     <div className="min-h-screen flex flex-col">
       <Header />
 
-      <main className="flex-1 pt-[70px]">
+      <main id="main-content" tabIndex={-1} className="flex-1 pt-16 outline-none lg:pt-[70px]">
         <DemoBanner />
         {/* Hero */}
         <PageHero
